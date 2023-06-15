8e4c01a8898ddc4aa301ec80ee8fe909a4002d44
unknown <senhe.zhou@fenbeitong.com>
2023-06-15 20:46:52 +0800

    [feature]:opt

---
+++ dist/utils/index.d.ts
@@ -0,0 +1,8 @@
+export declare const isObject: (value: unknown) => boolean;
+export declare const isArray: (value: unknown) => boolean;
+export declare const isFunction: (value: unknown) => boolean;
+export declare const isString: (value: unknown) => boolean;
+export declare const isBoolean: (value: unknown) => boolean;
+export declare const isNumber: (value: unknown) => boolean;
+export declare const isUndef: (value: unknown) => boolean;
+export declare function isTypeSame(param1: unknown, param2: unknown): boolean;
