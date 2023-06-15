4ac1157de6517fac3f1a1c624812ab2f3f2d85d9
unknown <senhe.zhou@fenbeitong.com>
2023-06-15 23:00:18 +0800

    [feature]:fix resetState arg

--- src/useMemoState/index.ts
+++ src/useMemoState/index.ts
@@ -1,16 +1,17 @@
-import { useState, useRef, Dispatch, SetStateAction } from 'react'
-import useMerge from "../useMerge";
+import { useState, useRef, Dispatch, SetStateAction } from 'react';
+import useMerge from '../useMerge';
 
-
-export default function useMemoState<T>(params: T | (() => T)): [T, Dispatch<SetStateAction<T>>, (arg0: any) => void] {
-    const { current: initalState } = useRef(params);
-    const [state, setState] = useState(initalState);
-    const { current: resetState } = useRef((otherParams: any) => {
-        if (otherParams) {
-            setState(useMerge(initalState, otherParams));
-        } else {
-            setState(initalState);
-        }
-    });
-    return [state, setState, resetState];
+export default function useMemoState<T>(
+  params: T | (() => T),
+): [T, Dispatch<SetStateAction<T>>, (arg0: any) => void] {
+  const { current: initalState } = useRef(params);
+  const [state, setState] = useState(initalState);
+  const { current: resetState } = useRef((otherParams?: any) => {
+    if (otherParams) {
+      setState(useMerge(initalState, otherParams));
+    } else {
+      setState(initalState);
+    }
+  });
+  return [state, setState, resetState];
 }
